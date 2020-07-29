e0e240f2fe6b383a0a57e3b837c5e5504947c343
PrestonWestDev <57919582+PrestonWestDev@users.noreply.github.com>
2020-07-29 15:28:29 -0500

    Progress on Changing Table Size feature

--- src/app/components/game-options/game-options.component.ts
+++ src/app/components/game-options/game-options.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-options',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-options.component.scss']
 })
 export class GameOptionsComponent implements OnInit {
+  @Output() changeBoardSize: EventEmitter<any> = new EventEmitter();
+  rowSize: number;
+  columnSize: number;
+  rowSizes: number[] = [5, 10, 25, 50, 75, 100];
+  colSizes: number[] = [5, 10, 25, 50]
 
   constructor() { }
 
@@ -14,6 +19,35 @@ export class GameOptionsComponent implements OnInit {
 
   addBlok() {
     console.log("adding blok!");
+  }
+
+
+  changeColSize(event) {
+    console.log("Changing Column Size")
+    console.log(event.target.value);
+
+    this.columnSize = event.target.value;
+
+    const boardSize = {
+      columnSize: this.columnSize,
+      rowSize: this.rowSize
+    }
+
+    this.changeBoardSize.emit(boardSize);
+  }
+
+  changeRowSize(event) {
+    console.log("Changing Row Size")
+    console.log(event.target.value);
+
+    this.rowSize = event.target.value;
+    
+    const boardSize = {
+      columnSize: this.columnSize,
+      rowSize: this.rowSize
+    }
+
+    this.changeBoardSize.emit(boardSize);
   }
 
 }
--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -19,4 +19,24 @@ export class BoardService {
         [{column: 0, row: 2, type: 'grass'},{column: 1, row: 2, type: 'dirt'},{column: 2, row: 2, type: 'dirt'}],
       ]
     }
+
+    getCleanBoard(columns: number, rows: number): any[] {
+      var i;
+      var j;
+      var arr = [];
+      for (i=0; i < 20; i++) {
+        let newArr = []
+        for (j=0; j < 20; j++) {
+          var type = Math.floor(Math.random() * 10) < 7 ? 'grass' : 'dirt';
+          let newObj = {
+            column: i,
+            row: j,
+            type: type,
+          }
+          newArr.push(newObj)
+        }
+        arr.push(newArr);
+      }
+      return arr;
+    }
   }
